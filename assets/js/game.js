9379221b48a25d193d0bb649826129bc3981df09
Riley Rasmussen <rileyras@hotmail.com>
2021-08-05 10:57:43 -0600

    added enemy combatants, need to fix for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,13 +6,17 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}
 
-
-var fight = function() {
+var fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -48,6 +52,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   
   // if player choses to skip
   } else if (promptFight === "skip" || promptFight === "SKIP") {
+
   // confirm player wants to skip
   var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -59,9 +64,19 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   }
   // if no (false), ask question again by running fight() again
   else {
-    fight();
+    var fight = function(enemyName) {
+     // fight function statements
+    };
   }
 }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
